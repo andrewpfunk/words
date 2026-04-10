8b1515d6d363755883ff31616d39d1b39a14bd5c
Andrew P Funk <andrewpfunk@gmail.com>
2026-04-10 14:38:53 -0400

    update words.js

--- words.js
+++ words.js
@@ -3,7 +3,10 @@ fetch('words.txt')
   .then(response => response.text())
   .then(text => {
     const wordSet = new Set(text.split(/\r?\n/));
+
+    document.getElementById("statusSpan").innerText = "Loading...Done!";    
+    console.log("Words loaded:");
+    console.log(wordSet.size);
     console.log(wordSet.has('century'));
   });
 
-document.getElementById("statusSpan").innerText = "Loading...Done!";
