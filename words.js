b1968287cca7e28449013ebd8490afdadf7615a9
Andrew P Funk <andrewpfunk@gmail.com>
2026-04-10 13:43:57 -0400

    Fetch words.txt into a Set

--- words.js
+++ words.js
@@ -1,2 +1,9 @@
 
+fetch('words.txt')
+  .then(response => response.text())
+  .then(text => {
+    const wordSet = new Set(text.split(/\r?\n/));
+    console.log(wordSet.has('example text'));
+  });
+
 document.getElementById("statusSpan").innerText = "Loading...Done!";
