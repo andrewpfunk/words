d563e799e42fedf8c5907b86cc23a6e4247693ac
Andrew P Funk <andrewpfunk@gmail.com>
2026-04-10 15:19:19 -0400

    add function to change one letter

--- words.js
+++ words.js
@@ -4,9 +4,17 @@ fetch('words.txt')
   .then(text => {
     const wordSet = new Set(text.split(/\r?\n/));
 
-    document.getElementById("statusSpan").innerText = "Loading...Done!";    
-    console.log("Words loaded:");
-    console.log(wordSet.size);
-    console.log(wordSet.has('century'));
+    document.getElementById("statusSpan").innerText += "Ready!";    
+    console.log("Words loaded: " + wordSet.size);
+    
+    document.getElementById("changeOneButton").addEventListener("click", () => {
+      const inputWord = document.getElementById("wordInput").value.trim().toLowerCase();
+      if (wordSet.has(inputWord)) {
+        document.getElementById("resultSpan").innerText = `"${inputWord}" is a valid word!`;
+      } else {
+        document.getElementById("resultSpan").innerText = `"${inputWord}" is not a valid word.`;
+      }
+    }); 
+    
   });
 
