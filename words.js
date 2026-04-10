05fbe45f525ae84cf13d06fc7f4501dbd5974573
Andrew P Funk <andrewpfunk@gmail.com>
2026-04-10 14:24:23 -0400

    test for a word that is in list

--- words.js
+++ words.js
@@ -3,7 +3,7 @@ fetch('words.txt')
   .then(response => response.text())
   .then(text => {
     const wordSet = new Set(text.split(/\r?\n/));
-    console.log(wordSet.has('example text'));
+    console.log(wordSet.has('century'));
   });
 
 document.getElementById("statusSpan").innerText = "Loading...Done!";
